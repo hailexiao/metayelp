6f1260923c17a9346f042046a944ad0a01d57e49
Brendan Boyle <brendanboyle87@gmail.com>
2015-09-21 15:44:13 -0400

    add review model and unit tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921154612) do
+ActiveRecord::Schema.define(version: 20150921191020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+=======
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.integer  "rating",     null: false
+    t.integer  "user_id",    null: false
+    t.integer  "yelper_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
