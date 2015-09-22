58112eacdcc5362c3a8d2b0efcd496e08f946300
Haile Xiao <haile.xiao@gmail.com>
2015-09-22 11:13:34 -0400

    finished yelper model and model test

--- spec/models/yelper_spec.rb
+++ spec/models/yelper_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Yelper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :reviews }
+
+  it { should validate_presence_of :name}
+
+  it { should validate_presence_of :number_of_reviews}
+  it { should validate_numericality_of :number_of_reviews}
+
+  it { should validate_presence_of :image_url }
+
+  it { should validate_presence_of :uid }
 end
