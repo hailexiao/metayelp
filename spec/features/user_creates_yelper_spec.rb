8c1f1e36ee0d0e88ecc265b7851e2e283616c887
Leise <email@estc.la>
2015-09-22 16:30:15 -0400

    Update Yelper controller.

--- spec/features/user_creates_yelper_spec.rb
+++ spec/features/user_creates_yelper_spec.rb
@@ -10,15 +10,14 @@ feature 'user creates yelper', %{
   scenario 'successfully submits yelper' do
     link_url = "http://www.yelp.com/user_details?userid=grEg3_xe95VezJytyov7cQ"
     visit new_yelper_path
-    fill_in 'yelper[uid]',
-      with: link_url
+    fill_in 'yelper[uid]', with: link_url
     click_button 'Submit'
 
     doc = Nokogiri::HTML(open(
-                          link_url,
-                          "User-Agent" => "Ruby/#{RUBY_VERSION}",
-                          "From" => "foo@bar.invalid",
-                          "Referer" => "http://www.ruby-lang.org/"))
+                              link_url,
+                              "User-Agent" => "Ruby/#{RUBY_VERSION}",
+                              "From" => "foo@bar.invalid",
+                              "Referer" => "http://www.ruby-lang.org/"))
 
     location = doc.css(".user-location").text
     name = doc.css(".user-profile_info h1").text
