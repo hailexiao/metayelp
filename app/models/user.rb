b441fc97932183581caf5261b87fd58ac30b8a77
Haile Xiao <haile.xiao@gmail.com>
2015-09-23 13:35:42 -0400

    fix syntax error in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many: :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
