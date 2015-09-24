602c1397e4da1f6dc2e9c768185d105376008fdf
Brendan Boyle <brendanboyle87@gmail.com>
2015-09-24 10:22:24 -0400

    pass test for making users are signed in prior to adding a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @review = Review.new(review_params)
     @yelper = Yelper.find(params[:yelper_id])
     @review.yelper = @yelper
+
+    if @review.save
+      flash[:notice] = 'Review added!'
+      redirect_to yelper_path(@yelper)
+    else
+      redirect_to yelper_path(@yelper)
+    end
   end
 
   private
 
   def review_params
-    parmas.require(:review).permit(:rating, :body)
+    params.require(:review).permit(:rating, :body)
   end
 end
