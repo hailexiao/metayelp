c52e000f9908a69de064e1e528e7d58907248ffa
Haile Xiao <haile.xiao@gmail.com>
2015-09-25 15:15:52 -0400

    feed the hound fifth time

--- config/initializers/simple_form_foundation.rb
+++ config/initializers/simple_form_foundation.rb
@@ -1,44 +1,37 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
 
-  config.wrappers
-    :vertical_form,
-    class: :input,
-    hint_class: :field_with_hint,
-    error_class: :error do |b|
-      b.use :html5
-      b.use :placeholder
-      b.optional :maxlength
-      b.optional :pattern
-      b.optional :min_max
-      b.optional :readonly
-      b.use :label_input
-      b.use :error, wrap_with: { tag: :small, class: :error }
+  config.wrappers :vertical_form,class: :input,hint_class: :field_with_hint,
+                  error_class: :error do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label_input
+    b.use :error, wrap_with: { tag: :small, class: :error }
   end
 
-  config.wrappers
-    :horizontal_form,
-    tag: 'div',
-    class: 'row',
-    hint_class: :field_with_hint,
-    error_class: :error do |b|
-      b.use :html5
-      b.use :placeholder
-      b.optional :maxlength
-      b.optional :pattern
-      b.optional :min_max
-      b.optional :readonly
+  config.wrappers :horizontal_form,tag: 'div',class: 'row',
+                  hint_class: :field_with_hint,error_class: :error do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
 
-      b.wrapper :label_wrapper, tag: :div, class: 'small-3 columns' do |ba|
-        ba.use :label, class: 'right inline'
-      end
+    b.wrapper :label_wrapper, tag: :div, class: 'small-3 columns' do |ba|
+      ba.use :label, class: 'right inline'
+    end
 
-      b.wrapper
-        :right_input_wrapper,
-        tag: :div,
-        class: 'small-9 columns' do |ba|
-          ba.use :input
-          ba.use :error, wrap_with: { tag: :small, class: :error }
+    b.wrapper
+      :right_input_wrapper,
+      tag: :div,
+      class: 'small-9 columns' do |ba|
+        ba.use :input
+        ba.use :error, wrap_with: { tag: :small, class: :error }
       end
   end
 
@@ -65,23 +58,19 @@ SimpleForm.setup do |config|
   # Note that you need to adapt this wrapper to your needs. If you need a 4
   # columns form then change the wrapper class to 'small-3', if you need
   # only two use 'small-6' and so on.
-  config.wrappers
-    :inline_form,
-    tag: 'div',
-    class: 'column small-4',
-    hint_class: :field_with_hint,
-    error_class: :error do |b|
-      b.use :html5
-      b.use :placeholder
-      b.optional :maxlength
-      b.optional :pattern
-      b.optional :min_max
-      b.optional :readonly
+  config.wrappers :inline_form,tag: 'div',class: 'column small-4',
+                  hint_class: :field_with_hint,error_class: :error do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
 
-      b.use :label, class: 'hidden-for-small-up'
-      b.use :input
+    b.use :label, class: 'hidden-for-small-up'
+    b.use :input
 
-      b.use :error, wrap_with: { tag: :small, class: :error }
+    b.use :error, wrap_with: { tag: :small, class: :error }
   end
 
   config.wrappers :customizable_wrapper, tag: 'div', error_class: :error do |b|
