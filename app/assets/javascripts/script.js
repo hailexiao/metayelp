3117ca8dc1d02dc4ed68e0a1acd587c8ab27e145
Leise <email@estc.la>
2015-10-01 15:51:48 -0400

    More hound edits.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,6 +1,5 @@
 $('#up-vote').on('click', function(e) {
       e.preventDefault();
-      debugger;
       var $this = $(this);
       var reviewId = $this.attr('data-review-id');
       var yelperId = $this.attr('data-yelper-id');
@@ -10,7 +9,7 @@ $('#up-vote').on('click', function(e) {
          url: '/yelpers/' + yelperId + '/reviews/' + reviewId + '/upvotes',
          dataType: 'json'
        }).done(function(resp) {
-          div = $this.parent();
+          var div = $this.parent();
           div.find('.upvote-count').html(resp.upvotesCount);
           div.find('.downvote-count').html(resp.downvotesCount);
        });
