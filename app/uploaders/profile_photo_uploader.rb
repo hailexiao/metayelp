739007d9291e0c51c0d224eae9e34d675f081727
Leise <email@estc.la>
2015-10-01 16:50:51 -0400

    Fixed hound errors.

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -13,7 +13,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png bmp)
   end
 
-  def default_url(*args)
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  def default_url(*)
+    ActionController::Base.helpers.asset_path(
+      "fallback/" + [version_name, "default.png"].compact.join('_'))
   end
 end
