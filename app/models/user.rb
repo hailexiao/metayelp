0dc37dad243c29b709494b865f5abca4b9ce7f35
Leise <email@estc.la>
2015-10-01 11:20:45 -0400

    add pagination to yelpers show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+
+  IMAGE_SOURCES = [
+    "http://www.placebear.com/250/250",
+    "http://placekitten.com/g/250/250",
+    "http://placecreature.com/panda/250/250",
+    "http://placecreature.com/kitten/250/250",
+    "http://placecreature.com/bear/250/250"
+  ]
+
   has_many :reviews, dependent: :destroy
   has_many :upvotes, dependent: :destroy
   has_many :downvotes, dependent: :destroy
@@ -10,5 +19,12 @@ class User < ActiveRecord::Base
 
   def admin?
     role == "admin"
+  end
+
+  def display_image
+    if profile_photo
+      profile_photo
+    end
+    IMAGE_SOURCES.sample
   end
 end
